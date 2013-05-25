150e0e9c5f7a8fd3776268bbac384713f8849643
Andrei Popescu <popescu.andrei1991@gmail.com>
2013-05-25 09:48:33 +0300

    added makefile, created a basic echo http_server

---
+++ linux/main_http_server.c
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "util.h"
+#include "debug.h"
+#include "sock_util.h"
+#include "w_epoll.h"
+
+#define ECHO_LISTEN_PORT		42424
+
+/* server socket file descriptor */
+static int listenfd;
+
+/* epoll file descriptor */
+static int epollfd;
+
+enum connection_state {
+	STATE_DATA_RECEIVED,
+	STATE_DATA_SENT,
+	STATE_CONNECTION_CLOSED
+};
+
+/* structure acting as a connection handler */
+struct connection {
+	int sockfd;
+	/* buffers used for receiving messages and then echoing them back */
+	char recv_buffer[BUFSIZ];
+	size_t recv_len;
+	char send_buffer[BUFSIZ];
+	size_t send_len;
+	enum connection_state state;
+};
+
+static struct connection *connection_create(int sockfd)
+{
+	struct connection *conn = malloc(sizeof(*conn));
+	DIE(conn == NULL, "malloc");
+
+	conn->sockfd = sockfd;
+	memset(conn->recv_buffer, 0, BUFSIZ);
+	memset(conn->send_buffer, 0, BUFSIZ);
+
+	return conn;
+}
+
+/*
+ * Copy receive buffer to send buffer (echo).
+ */
+
+static void connection_copy_buffers(struct connection *conn)
+{
+	conn->send_len = conn->recv_len;
+	memcpy(conn->send_buffer, conn->recv_buffer, conn->send_len);
+}
+
+/*
+ * Remove connection handler.
+ */
+
+static void connection_remove(struct connection *conn)
+{
+	close(conn->sockfd);
+	conn->state = STATE_CONNECTION_CLOSED;
+	free(conn);
+}
+
+/*
+ * Handle a new connection request on the server socket.
+ */
+
+static void handle_new_connection(void)
+{
+	static int sockfd;
+	socklen_t addrlen = sizeof(struct sockaddr_in);
+	struct sockaddr_in addr;
+	struct connection *conn;
+	int rc;
+
+	/* accept new connection */
+	sockfd = accept(listenfd, (SSA *) &addr, &addrlen);
+	DIE(sockfd < 0, "accept");
+
+	dlog(LOG_ERR, "Accepted connection from: %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+
+	/* instantiate new connection handler */
+	conn = connection_create(sockfd);
+
+	/* add socket to epoll */
+	rc = w_epoll_add_ptr_in(epollfd, sockfd, conn);
+	DIE(rc < 0, "w_epoll_add_in");
+}
+
+/*
+ * Receive message on socket.
+ * Store message in recv_buffer in struct connection.
+ */
+
+static enum connection_state receive_message(struct connection *conn)
+{
+	ssize_t bytes_recv;
+	int rc;
+	char abuffer[64];
+
+	rc = get_peer_address(conn->sockfd, abuffer, 64);
+	if (rc < 0) {
+		ERR("get_peer_address");
+		goto remove_connection;
+	}
+
+	bytes_recv = recv(conn->sockfd, conn->recv_buffer, BUFSIZ, 0);
+	if (bytes_recv < 0) {		/* error in communication */
+		dlog(LOG_ERR, "Error in communication from: %s\n", abuffer);
+		goto remove_connection;
+	}
+	if (bytes_recv == 0) {		/* connection closed */
+		dlog(LOG_INFO, "Connection closed from: %s\n", abuffer);
+		goto remove_connection;
+	}
+
+	dlog(LOG_DEBUG, "Received message from: %s\n", abuffer);
+
+	printf("--\n%s--\n", conn->recv_buffer);
+
+	conn->recv_len = bytes_recv;
+	conn->state = STATE_DATA_RECEIVED;
+
+	return STATE_DATA_RECEIVED;
+
+remove_connection:
+	rc = w_epoll_remove_ptr(epollfd, conn->sockfd, conn);
+	DIE(rc < 0, "w_epoll_remove_ptr");
+
+	/* remove current connection */
+	connection_remove(conn);
+
+	return STATE_CONNECTION_CLOSED;
+}
+
+/*
+ * Send message on socket.
+ * Store message in send_buffer in struct connection.
+ */
+
+static enum connection_state send_message(struct connection *conn)
+{
+	ssize_t bytes_sent;
+	int rc;
+	char abuffer[64];
+
+	rc = get_peer_address(conn->sockfd, abuffer, 64);
+	if (rc < 0) {
+		ERR("get_peer_address");
+		goto remove_connection;
+	}
+
+	bytes_sent = send(conn->sockfd, conn->send_buffer, conn->send_len, 0);
+	if (bytes_sent < 0) {		/* error in communication */
+		dlog(LOG_ERR, "Error in communication to %s\n", abuffer);
+		goto remove_connection;
+	}
+	if (bytes_sent == 0) {		/* connection closed */
+		dlog(LOG_INFO, "Connection closed to %s\n", abuffer);
+		goto remove_connection;
+	}
+
+	dlog(LOG_DEBUG, "Sending message to %s\n", abuffer);
+
+	printf("--\n%s--\n", conn->send_buffer);
+
+	/* all done - remove out notification */
+	rc = w_epoll_update_ptr_in(epollfd, conn->sockfd, conn);
+	DIE(rc < 0, "w_epoll_update_ptr_in");
+
+	conn->state = STATE_DATA_SENT;
+
+	return STATE_DATA_SENT;
+
+remove_connection:
+	rc = w_epoll_remove_ptr(epollfd, conn->sockfd, conn);
+	DIE(rc < 0, "w_epoll_remove_ptr");
+
+	/* remove current connection */
+	connection_remove(conn);
+
+	return STATE_CONNECTION_CLOSED;
+}
+
+/*
+ * Handle a client request on a client connection.
+ */
+
+static void handle_client_request(struct connection *conn)
+{
+	int rc;
+	enum connection_state ret_state;
+
+	ret_state = receive_message(conn);
+	if (ret_state == STATE_CONNECTION_CLOSED)
+		return;
+
+	connection_copy_buffers(conn);
+
+	/* add socket to epoll for out events */
+	rc = w_epoll_update_ptr_inout(epollfd, conn->sockfd, conn);
+	DIE(rc < 0, "w_epoll_add_ptr_inout");
+}
+
+int main() {
+return 0;
+}
